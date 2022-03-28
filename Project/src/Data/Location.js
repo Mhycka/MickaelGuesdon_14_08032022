031b97a6de8c9da8e30c38fb072bf1cb3636b7c0
Guesdon Mickael <75080138+Mhycka@users.noreply.github.com>
2022-03-28 14:16:53 +0200

    Delete Project/src directory

--- Project/src/Data/Location.js
+++
@@ -1,6 +0,0 @@
-export const Departments = [
-    { value: "sales", text: "Sales" },
-    { value: "engineering", text: "Engineering" },
-    { value: "hr", text: "Human Resources" },
-    { value: "legal", text: "Legal" },
-  ];
